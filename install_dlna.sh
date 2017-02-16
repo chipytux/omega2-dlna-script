806e49e3a5ce9382f92f3483585d01fd811172ef
chipytux <chipytux@users.noreply.github.com>
2017-02-16 15:06:08 -0300

    Add files via upload

---
+++ install_dlna.sh
@@ -0,0 +1,6 @@
+wget http://downloads.lede-project.org/snapshots/packages/mipsel_24kc/packages/minidlna_1.1.6+git-8a996b-1_mipsel_24kc.ipk
+wget http://downloads.lede-project.org/snapshots/packages/mipsel_24kc/packages/libexif_0.6.21-1_mipsel_24kc.ipk
+opkg install minidlna_1.1.6+git-8a996b-1_mipsel_24kc.ipk libexif_0.6.21-1_mipsel_24kc.ipk --force-depends
+cp minidlna.conf > /etc/minidlna.conf
+echo "minidlna" >> /etc/rc.local
+reboot
